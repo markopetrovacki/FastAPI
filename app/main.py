75e0c6ea665cbbd52493025f4dfb88def7e34506
markopetrovacki <petrovacki9@gmail.com>
2025-05-08 12:00:39 +0200

    Kreiranje migracija

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote, firma, roba, usluga, racun, kupac
@@ -9,6 +11,16 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[],
+   # allow_origins=["https://mojfrontend.netlify.app"],  # ili ["*"] za   mozes da dodas i dva servisa npr google i youtube
+   #OVO JE KADA NAMESTIS FRONTEND DA DODAS NJEGOVU PUDANJU DA BI MOGLI NESMETANO DA SALJU API ZAHTEVE INACE BEZ TOGA NE BI MOGLI
+   # OVO NEMA VEZE SA CLOUDOM DA NA ISTOM CLOUDU MOGU DA LOMUNICIRAJU VEĆ SA NJIHOVIM URL-om NA KOJEM SE POKRECU 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
     
 #while True:
 #    try:
@@ -33,8 +45,8 @@ app.include_router(usluga.router)
 app.include_router(racun.router)
 
 
-#@app.get("/")
-#async def root():
-#    return {"message": "Hello World"}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
